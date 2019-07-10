c74695155dcc6e01968e017a36c76efc3de4b445
Edcarl Adraincem <iamedcarl@users.noreply.github.com>
2019-07-09 22:31:47 -0700

    Create mutation to post link

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-	type Query {
-		info: String!
-		feed: [Link!]!
-	}
-
-	type Link {
-		id: ID!
-		description: String!
-		url: String!
-	}
-`
-
 // Temporaryily storing in-memory rather than being persisted in a database
 let links = [
 	{
@@ -27,10 +14,23 @@ let links = [
 	}
 ]
 
+let idCount = links.length
+
 const resolvers = {
 	Query: {
 		info: () => 'This is the API of a Hackernews Clone',
 		feed: () => links,
+	},
+	Mutation: {
+		post: (parent, args) => {
+			const link = {
+				id: `link-${idCount++}`,
+				description: args.description,
+				url: args.url,
+			}
+			links.push(link)
+			return link
+		}
 	},
 	Link: {
 		id: (parent) => parent.id,
@@ -40,7 +40,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({
-	typeDefs,
+	typeDefs: './src/schema.graphql',
 	resolvers,
 })
 server.start(() => console.log('Server is running on http://localhost:4000'))
