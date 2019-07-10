281d7b888c464cbdda0f093bd3942e963afa72f4
Edcarl Adraincem <iamedcarl@users.noreply.github.com>
2019-07-09 22:00:31 -0700

    Extend schema definition to include links

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-// 1
 const typeDefs = `
 	type Query {
 		info: String!
+		feed: [Link!]!
+	}
+
+	type Link {
+		id: ID!
+		description: String!
+		url: String!
 	}
 `
 
-// 2
+// Temporaryily storing in-memory rather than being persisted in a database
+let links = [
+	{
+		id: 'link-0',
+		url: 'www.howtographql.com',
+		description: 'Fullstack Tutorial for GraphQL'
+	},
+	{
+		id: 'link-1',
+		url: 'www.edcarladraincem.com',
+		description: 'Personal Site of Edcarl Adraincem'
+	}
+]
+
 const resolvers = {
 	Query: {
-		info: () => 'This is the API of a Hackernews Clone'
+		info: () => 'This is the API of a Hackernews Clone',
+		feed: () => links,
+	},
+	Link: {
+		id: (parent) => parent.id,
+		description: (parent) => parent.description,
+		url: (parent) => parent.url,
 	}
 }
 
-// 3
 const server = new GraphQLServer({
 	typeDefs,
 	resolvers,
